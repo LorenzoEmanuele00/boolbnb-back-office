8dd01ed846eceede31898a43bea150c285991dd8
PapiMatteo <matteopapi.work@gmail.com>
2024-02-19 18:14:22 +0100

    fix: index

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -17,7 +17,7 @@ class ApartmentController extends Controller
      */
     public function index()
     {
-        $apartments = Apartment::where('user_id', '=', Auth::user()->id);
+        $apartments = Apartment::where('user_id', '=', Auth::user()->id)->get();
         return view('admin.apartments.index', compact('apartments'));
     }
 
