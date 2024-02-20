481b8809a2d839e412ae4041a5e494ea8357e7ba
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2024-02-20 16:07:27 +0100

    Update: create form

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -16,7 +16,9 @@
 
                     <div class="mb-3 has-validation">
                         <label for="title" class="form-label">Titolo</label>
-                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+                        <input type="text" minlength="5" maxlength="150" required
+                            class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                            value="{{ old('title') }}">
                     </div>
                     @error('title')
                         <div class="alert alert-danger">
@@ -26,7 +28,9 @@
 
                     <div class="mb-3">
                         <label for="price" class="form-label">Prezzo</label>
-                        <input type="number" step=".01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}">
+                        <input type="number" required step=".01"
+                            class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                            value="{{ old('price') }}">
                     </div>
                     @error('price')
                         <div class="alert alert-danger">
@@ -36,7 +40,8 @@
 
                     <div class="mb-3">
                         <label for="address" class="form-label">Indirizzo</label>
-                        <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address') }}">
+                        <input type="text" required class="form-control @error('address') is-invalid @enderror"
+                            id="address" name="address" value="{{ old('address') }}">
                     </div>
                     @error('address')
                         <div class="alert alert-danger">
@@ -46,7 +51,8 @@
 
                     <div class="mb-3">
                         <label for="dimension_mq" class="form-label">Dimensione m²</label>
-                        <input type="number" class="form-control @error('dimension_mq') is-invalid @enderror" id="dimension_mq" name="dimension_mq" value="{{ old('dimension_mq') }}">
+                        <input type="number" required class="form-control @error('dimension_mq') is-invalid @enderror"
+                            id="dimension_mq" name="dimension_mq" value="{{ old('dimension_mq') }}">
                     </div>
                     @error('dimension_mq')
                         <div class="alert alert-danger">
@@ -56,7 +62,8 @@
 
                     <div class="mb-3">
                         <label for="rooms_number" class="form-label">Numero di Camere</label>
-                        <input type="number" class="form-control @error('rooms_number') is-invalid @enderror" id="rooms_number" name="rooms_number" value="{{ old('rooms_number') }}">
+                        <input type="number" required class="form-control @error('rooms_number') is-invalid @enderror"
+                            id="rooms_number" name="rooms_number" value="{{ old('rooms_number') }}">
                     </div>
                     @error('rooms_number')
                         <div class="alert alert-danger">
@@ -66,7 +73,8 @@
 
                     <div class="mb-3">
                         <label for="beds_number" class="form-label">Numero di Letti</label>
-                        <input type="number" class="form-control @error('beds_number') is-invalid @enderror" id="beds_number" name="beds_number" value="{{ old('beds_number') }}">
+                        <input type="number" required class="form-control @error('beds_number') is-invalid @enderror"
+                            id="beds_number" name="beds_number" value="{{ old('beds_number') }}">
                     </div>
                     @error('beds_number')
                         <div class="alert alert-danger">
@@ -76,7 +84,8 @@
 
                     <div class="mb-3">
                         <label for="bathrooms_number" class="form-label">Numero di Bagni</label>
-                        <input type="number" class="form-control @error('bathrooms_number') is-invalid @enderror" id="bathrooms_number" name="bathrooms_number" value="{{ old('bathrooms_number') }}">
+                        <input type="number" required class="form-control @error('bathrooms_number') is-invalid @enderror"
+                            id="bathrooms_number" name="bathrooms_number" value="{{ old('bathrooms_number') }}">
                     </div>
                     @error('bathrooms_number')
                         <div class="alert alert-danger">
@@ -86,18 +95,24 @@
 
                     <div class="mb-3">
                         <label for="is_visible">Disponibilità</label>
-                        <select class="form-select" name="is_visible" id="is_visible">
+                        <select class="form-select" required name="is_visible" id="is_visible">
                             <option @selected(!old('is_visible')) value="">Nessuna tipologia</option>
                             <option @selected(old('is_visible') === 0) value="0">Disponibile</option>
                             <option @selected(old('is_visible') === 1) value="1">Non Disponibile</option>
                         </select>
                     </div>
 
-                    {{-- <div class="mb-3">
+                    @error('is_visible')
+                        <div class="alert alert-danger">
+                            <div>{{ $message }}</div>
+                        </div>
+                    @enderror
+
+                    <div class="mb-3">
                         <label for="cover_image" class="form-label">Immagine</label>
-                        <input class="form-control" type="file" id="cover_image" name="cover_image"
-                            value="{{ old('cover_image') }}">
-                    </div> --}}
+                        <input class="form-control" type="file" multiple id="cover_image" name="cover_image[]" value="{{ old('cover_image') }}">
+                        <p class="alert alert-warning p-2 mt-2">Per selezionare più file, tenere premuto il tasto Ctrl (o Cmd su Mac) mentre si selezionano i file.</p>
+                    </div>
 
                     <button class="btn btn-success" type="submit">Salva</button>
 
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header red-bg">{{ __('Reset Password') }}</div>
+                <div class="card-header text-light red-bg">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -52,7 +52,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn red-bg">
+                                <button type="submit" class="btn btn-outline-danger text-light red-bg">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
