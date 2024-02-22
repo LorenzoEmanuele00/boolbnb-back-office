8822a87e7485645158e1fc2b353bcc4460526192
PapiMatteo <matteopapi.work@gmail.com>
2024-02-22 12:21:31 +0100

    add: search and filter function (WIP)

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class ApartmentController extends Controller
 {
@@ -37,6 +38,46 @@ class ApartmentController extends Controller
 
     }
 
-    
+    // public function searchFilter(Request $request) 
+    // {
+
+    //     $query = Apartment::query();
+
+    //     if ($request->filled('title')) {
+    //         $query->where('title', 'like', '%' . $request->input('title') . '%');
+    //     }
+
+    //     if ($request->filled('address')) {
+    //         $query->where('address', $request->input('address'));
+    //     }
+
+    //     if ($request->filled('price_min')) {
+    //         $query->where('price', '>=', $request->input('price_min'));
+    //     }
+
+    //     if ($request->filled('price_max')) {
+    //         $query->where('price', '<=', $request->input('price_max'));
+    //     }
+
+    //     if ($request->filled('people')) {
+    //         $query->where('beds_numbers', '>=', $request->input('people'));
+    //     }
+
+    //     if ($request->filled('rooms')) {
+    //         $query->where('rooms_numbers', '>=', $request->input('rooms'));
+    //     }
+
+    //     $services_selected = $request->input('services', []);
+
+    //     foreach ($services_selected as $service) {
+
+    //         $query->whereHas('services', function ($query) use ($service) {
+    //             $query->where('name', $service);
+    //         });
+    //     }
+    //     $appartamenti = $query->get();
+    //     return view('risultati-ricerca', ['appartamenti' => $appartamenti]);
+
+    // }
 
 }
