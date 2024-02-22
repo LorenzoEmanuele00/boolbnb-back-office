ebe69820dbb0d96a638b9dc5a46e517ed9f05de9
PapiMatteo <matteopapi.work@gmail.com>
2024-02-22 11:23:03 +0100

    add: api index function

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    //
+    public function index(){
+        $apartments = Apartment::with('images')->paginate(10);
+
+        return response()->json([
+            'results' => $apartments,
+            'success' => true
+        ]);
+    }
+
 }
