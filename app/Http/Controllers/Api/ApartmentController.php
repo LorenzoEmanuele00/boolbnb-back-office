8d9ac602937d93912f45eebf1a24bcf507827ff4
PapiMatteo <matteopapi.work@gmail.com>
2024-02-22 11:46:50 +0100

    add: api show function

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $apartments = Apartment::with('images')->paginate(10);
 
         return response()->json([
@@ -16,5 +17,26 @@ class ApartmentController extends Controller
             'success' => true
         ]);
     }
+
+    public function show(string $slug)
+    {
+
+        $apartment = Apartment::with('images', 'services')->where('slug', $slug)->first();
+
+        if($apartment) {
+            return response()->json([
+                'results' => $apartment,
+                'success' => true 
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Nessun appartamento trovato'
+            ]);
+        }
+
+    }
+
+    
 
 }
