545ffcfcb8d215afadf480790a6b3e9cf81b1bfa
PapiMatteo <matteopapi.work@gmail.com>
2024-02-26 17:41:15 +0100

    fix: api route

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/apartments', [ApartmentController::class, 'index']);
 Route::get('/apartment/{slug}', [ApartmentController::class, 'show']);
-Route::get('/services', ServiceController::class, 'index');
+Route::get('/services', [ServiceController::class, 'index']);
